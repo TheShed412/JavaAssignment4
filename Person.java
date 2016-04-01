718f552440013c7f50f7e2d723bc6218ea675c88
Tyler <shed412@gmail.com>
2016-04-01 01:15:51 -0400

    Added person class

---
+++ Person.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Person
+{
+  int id;
+  String name;
+  boolean voted;
+  
+  public Person(int id, String name, boolean voted)
+  {
+    this.id = id;
+    this.name = name;
+    this.voted = voted;
+  }//Person
+}//Person
